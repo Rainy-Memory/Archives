fab577d86619fa8a7a872e89a4a1b9310ee0ee6b
Rainy-Memory <2630737606@qq.com>
2021-06-02 22:28:22 +0800

    Library of RainyMemory

--- Mix/RTL/algorithm.h
+++ Mix/RTL/algorithm.h
@@ -13,10 +13,8 @@
  *
  */
 
-#include "vector.h"
-
 namespace RainyMemory {
-    namespace inner_vector {
+    namespace inner_sort {
         enum threshold {
             THRESHOLD = 16
         };
@@ -34,133 +32,295 @@ namespace RainyMemory {
         }
         
         template<class T>
-        void _make_heap(RainyMemory::vector<T> &vec, int low, int high, bool (*compare)(const T &, const T &)) {
+        void _make_heap(T *array, int low, int high, bool (*compare)(const T &, const T &)) {
             for (int i = low; i <= high; i++) {
                 int index = i - low + 1;
-                while ((index >> 1) >= 1 && compare(vec[(index >> 1) + low - 1], vec[index + low - 1])) _swap(vec[index + low - 1], vec[(index >> 1) + low - 1]), index >>= 1;
+                while ((index >> 1) >= 1 && compare(array[(index >> 1) + low - 1], array[index + low - 1])) _swap(array[index + low - 1], array[(index >> 1) + low - 1]), index >>= 1;
             }
         }
         
         template<class T>
-        void _pop_heap(RainyMemory::vector<T> &vec, int low, int high, bool (*compare)(const T &, const T &)) {
-            _swap(vec[low], vec[high]), high--;
+        void _pop_heap(T *array, int low, int high, bool (*compare)(const T &, const T &)) {
+            _swap(array[low], array[high]), high--;
             int now = 1, child;
             while ((now << 1) + low - 1 <= high) {
-                child = (now << 1 | 1) + low - 1 <= high && compare(vec[(now << 1) + low - 1], vec[(now << 1 | 1) + low - 1]) ? now << 1 | 1 : now << 1;
-                if (compare(vec[now + low - 1], vec[child + low - 1])) _swap(vec[now + low - 1], vec[child + low - 1]), now = child;
+                child = (now << 1 | 1) + low - 1 <= high && compare(array[(now << 1) + low - 1], array[(now << 1 | 1) + low - 1]) ? now << 1 | 1 : now << 1;
+                if (compare(array[now + low - 1], array[child + low - 1])) _swap(array[now + low - 1], array[child + low - 1]), now = child;
                 else break;
             }
         }
         
         template<class T>
-        void _sort_heap(RainyMemory::vector<T> &vec, int low, int high, bool (*compare)(const T &, const T &)) {
+        void _sort_heap(T *array, int low, int high, bool (*compare)(const T &, const T &)) {
             for (int i = high; i > low; i--) {
-                _pop_heap(vec, low, i, compare);
+                _pop_heap(array, low, i, compare);
             }
         }
         
         template<class T>
-        int _quick_sort_partition(RainyMemory::vector<T> &vec, int low, int high, bool (*compare)(const T &, const T &)) {
-            T temp = vec[low];
+        int _quick_sort_partition(T *array, int low, int high, bool (*compare)(const T &, const T &)) {
+            T temp = array[low];
             while (low < high) {
-                while (low < high && !(compare(vec[high], temp))) high--;
-                if (low < high) vec[low] = vec[high], low++;
-                while (low < high && (compare(vec[low], temp) || (!compare(vec[low], temp) && !compare(temp, vec[low])))) low++;
-                if (low < high) vec[high] = vec[low], high--;
+                while (low < high && !(compare(array[high], temp))) high--;
+                if (low < high) array[low] = array[high], low++;
+                while (low < high && (compare(array[low], temp) || (!compare(array[low], temp) && !compare(temp, array[low])))) low++;
+                if (low < high) array[high] = array[low], high--;
             }
-            vec[low] = temp;
+            array[low] = temp;
             return low;
         }
         
         template<class T>
-        void insertion_sort(RainyMemory::vector<T> &vec, int low, int high, bool (*compare)(const T &, const T &)) {
+        void insertion_sort(T *array, int low, int high, bool (*compare)(const T &, const T &)) {
             T temp;
             int j;
             for (int i = low + 1; i <= high; i++) {
-                temp = vec[i];
+                temp = array[i];
                 j = i - 1;
-                while (j >= low && compare(temp, vec[j])) vec[j + 1] = vec[j], j--;
-                vec[j + 1] = temp;
+                while (j >= low && compare(temp, array[j])) array[j + 1] = array[j], j--;
+                array[j + 1] = temp;
             }
         }
         
         template<class T>
-        void quick_sort(RainyMemory::vector<T> &vec, int low, int high, bool (*compare)(const T &, const T &)) {
+        void quick_sort(T *array, int low, int high, bool (*compare)(const T &, const T &)) {
             if (low >= high) return;
-            int mid = _quick_sort_partition(vec, low, high, compare);
-            quick_sort(vec, low, mid - 1, compare);
-            quick_sort(vec, mid + 1, high, compare);
+            int mid = _quick_sort_partition(array, low, high, compare);
+            quick_sort(array, low, mid - 1, compare);
+            quick_sort(array, mid + 1, high, compare);
         }
         
         template<class T>
-        void heap_sort(RainyMemory::vector<T> &vec, int low, int high, bool (*compare)(const T &, const T &)) {
-            _make_heap(vec, low, high, compare);
-            _sort_heap(vec, low, high, compare);
+        void heap_sort(T *array, int low, int high, bool (*compare)(const T &, const T &)) {
+            _make_heap(array, low, high, compare);
+            _sort_heap(array, low, high, compare);
         }
         
         template<class T>
-        void intro_sort(RainyMemory::vector<T> &vec, int low, int high, bool (*compare)(const T &, const T &), int depth_limitation) {
-            if (depth_limitation == 0) heap_sort(vec, low, high, compare);
-            else if (high - low < THRESHOLD) insertion_sort(vec, low, high, compare);
+        void intro_sort(T *array, int low, int high, bool (*compare)(const T &, const T &), int depth_limitation) {
+            if (depth_limitation == 0) heap_sort(array, low, high, compare);
+            else if (high - low < THRESHOLD) insertion_sort(array, low, high, compare);
             else {
-                int mid = _quick_sort_partition(vec, low, high, compare);
-                intro_sort(vec, low, mid - 1, compare, depth_limitation - 1);
-                intro_sort(vec, mid + 1, high, compare, depth_limitation - 1);
+                int mid = _quick_sort_partition(array, low, high, compare);
+                intro_sort(array, low, mid - 1, compare, depth_limitation - 1);
+                intro_sort(array, mid + 1, high, compare, depth_limitation - 1);
             }
-        }
-        
-        template<class iter>
-        void iter_swap(iter a, iter b) {
-            using std::swap;
-            swap(*a, *b);
         }
     }
     
     template<class T>
-    void sortVector(RainyMemory::vector<T> &vec, bool (*compare)(const T &, const T &) = [](const T &o1, const T &o2) -> bool { return o1 < o2; }) {
-        inner_vector::intro_sort(vec, 0, vec.size() - 1, compare, inner_vector::_lg(vec.size() - 1) * 2);
+    void old_sort(T *array, int start, int end, bool (*compare)(const T &, const T &) = [](const T &o1, const T &o2) -> bool { return o1 < o2; }) {
+        inner_sort::intro_sort(array, start, end, compare, inner_sort::_lg(end - start) * 2);
     }
     
-    template<class ptr, class T>
-    ptr lower_bound(ptr first, ptr last, const T &val) {
-        ptr now;
-        int count = last - first, step;
-        while (count > 0) {
-            step = count >> 1;
-            now = first + step;
-            if (*now < val) {
-                first = ++now;
-                count -= step + 1;
+    template<class ForwardIterator, class T>
+    ForwardIterator lower_bound(ForwardIterator first, ForwardIterator last, const T &val) {
+        ForwardIterator middle;
+        int len = last - first, half;
+        while (len > 0) {
+            half = len >> 1;
+            middle = first + half;
+            if (*middle < val) {
+                first = middle + 1;
+                len = len - half - 1;
             }
-            else count = step;
+            else len = half;
         }
         return first;
     }
     
-    template<class ptr, class T>
-    ptr upper_bound(ptr first, ptr last, const T &val) {
-        ptr now;
-        int count = last - first, step;
-        while (count > 0) {
-            step = count >> 1;
-            now = first + step;
-            if (!(val < *now)) {
-                first = ++now;
-                count -= step + 1;
+    template<class ForwardIterator, class T>
+    ForwardIterator upper_bound(ForwardIterator first, ForwardIterator last, const T &val) {
+        ForwardIterator middle;
+        int len = last - first, half;
+        while (len > 0) {
+            half = len >> 1;
+            middle = first + half;
+            if (val < *middle) len = half;
+            else {
+                first = middle + 1;
+                len = len - half - 1;
             }
-            else count = step;
         }
         return first;
     }
     
-    template<class ptr, class T>
-    ptr find(ptr first, ptr last, const T &val) {
-        int len = last - first;
-        for (int i = 0; i < len; i++, first++) {
-            if (!(val < *first) && !(*first < val)) return first;
-        }
+    template<class InputIterator, class T>
+    InputIterator find(InputIterator first, InputIterator last, const T &val) {
+        while (first != last && *first != val)first++;
         return last;
     }
+    
+    namespace sort_assist {
+        template<class RandomAccessIterator, class T>
+        void _adjust_heap(RandomAccessIterator first, int holeIndex, int len, const T &val) {
+            int secondChild = (holeIndex + 1) << 1;
+            while (secondChild < len) {
+                if (*(first + secondChild) < *(first + (secondChild - 1)))secondChild--;
+                *(first + holeIndex) = *(first + secondChild);
+                holeIndex = secondChild;
+                secondChild = (secondChild + 1) << 1;
+            }
+            if (secondChild == len) {
+                *(first + holeIndex) = *(first + (secondChild - 1));
+                holeIndex = secondChild - 1;
+            }
+            *(first + holeIndex) = val;
+        }
+        
+        template<class RandomAccessIterator, class T>
+        void _pop_heap(RandomAccessIterator first, RandomAccessIterator last, RandomAccessIterator result, const T &val) {
+            *result = *first;
+            _adjust_heap(first, 0, last - first, val);
+        }
+        
+        template<class RandomAccessIterator>
+        void pop_heap(RandomAccessIterator first, RandomAccessIterator last) {
+            _pop_heap(first, last - 1, last - 1, *(last - 1));
+        }
+        
+        template<class RandomAccessIterator>
+        void sort_heap(RandomAccessIterator first, RandomAccessIterator last) {
+            while (last - first > 1) {
+                pop_heap(first, last--);
+            }
+        }
+        
+        template<class RandomAccessIterator>
+        void make_heap(RandomAccessIterator first, RandomAccessIterator last) {
+            if (last - first < 2)return;
+            int len = last - first;
+            int parent = (len - 2) >> 1;
+            while (true) {
+                _adjust_heap(first, parent, len, *(first + parent));
+                if (parent == 0)return;
+                parent--;
+            }
+        }
+        
+        template<class RandomAccessIterator, class T>
+        void _partial_sort(RandomAccessIterator first, RandomAccessIterator middle, RandomAccessIterator last, const T &) {
+            make_heap(first, middle);
+            for (RandomAccessIterator i = middle; i < last; i++) {
+                if (*i < *first) {
+                    _pop_heap(first, middle, i, T(*i));
+                }
+            }
+            sort_heap(first, middle);
+        }
+        
+        template<class RandomAccessIterator>
+        inline void partial_sort(RandomAccessIterator first, RandomAccessIterator middle, RandomAccessIterator last) {
+            _partial_sort(first, middle, last, *first);
+        }
+        
+        template<class RandomAccessIterator, class T>
+        void _unguarded_linear_insert(RandomAccessIterator last, const T &val) {
+            RandomAccessIterator next = last;
+            next--;
+            while (val < *next) {
+                *last = *next;
+                last = next;
+                --next;
+            }
+            *last = val;
+        }
+        
+        template<class RandomAccessIterator, class T>
+        inline void _linear_insert(RandomAccessIterator first, RandomAccessIterator last, const T &) {
+            T val = *last;
+            if (val < *first) {
+                RandomAccessIterator d_last = last + 1;
+                while (first != last) {
+                    *(--d_last) = *(--last);
+                }
+                *first = val;
+            }
+            else _unguarded_linear_insert(last, val);
+        }
+        
+        template<class RandomAccessIterator>
+        void _insertion_sort(RandomAccessIterator first, RandomAccessIterator last) {
+            if (first == last)return;
+            for (RandomAccessIterator i = first + 1; i != last; i++) {
+                _linear_insert(first, i, *first);
+            }
+        }
+        
+        template<class T>
+        inline const T &_median(const T &a, const T &b, const T &c) {
+            if (a < b) {
+                if (b < c)return b;
+                else if (a < c)return c;
+                else return a;
+            }
+            else if (a < c)return a;
+            else if (b < c)return c;
+            else return b;
+        }
+        
+        template<class RandomAccessIterator, class T>
+        RandomAccessIterator _unguarded_partition(RandomAccessIterator first, RandomAccessIterator last, const T &pivot) {
+            while (true) {
+                while (*first < pivot)first++;
+                last--;
+                while (pivot < *last)last--;
+                if (!(first < last))return first;
+                std::swap(*first, *last);
+                first++;
+            }
+        }
+        
+        template<class Size>
+        inline Size _lg(Size n) {
+            Size k;
+            for (k = 0; n > 1; n >>= 1)k++;
+            return k;
+        }
+        
+        enum coefficient {
+            _threshold = 16
+        };
+        
+        template<class RandomAccessIterator, class T, class Size>
+        void _intro_sort_loop(RandomAccessIterator first, RandomAccessIterator last, const T &, Size depth_limit) {
+            while (last - first > _threshold) {
+                if (depth_limit == 0) {
+                    partial_sort(first, last, last);
+                    return;
+                }
+                depth_limit--;
+                RandomAccessIterator cut = _unguarded_partition(first, last, T(_median(*first, *(first + (last - first) / 2), *(last - 1))));
+                _intro_sort_loop(cut, last, *first, depth_limit);
+                last = cut;
+            }
+        }
+        
+        template<class RandomAccessIterator, class T>
+        inline void _unguarded_insertion_sort(RandomAccessIterator first, RandomAccessIterator last, const T &) {
+            for (RandomAccessIterator i = first; i != last; i++) {
+                _unguarded_linear_insert(i, T(*i));
+            }
+        }
+        
+        template<class RandomAccessIterator>
+        void _final_insertion_sort(RandomAccessIterator first, RandomAccessIterator last) {
+            if (last - first > _threshold) {
+                _insertion_sort(first, first + _threshold);
+                _unguarded_insertion_sort(first + _threshold, last, *first);
+            }
+            else _insertion_sort(first, last);
+        }
+    }
+    
+    template<class RandomAccessIterator>
+    inline void sort(RandomAccessIterator first, RandomAccessIterator last) {
+        if (first != last) {
+            sort_assist::_intro_sort_loop(first, last, *first, sort_assist::_lg(last - first) * 2);
+            sort_assist::_final_insertion_sort(first, last);
+        }
+    }
+    
 }
 
 #endif //TICKETSYSTEM_AUTOMATA_ALGORITHM_H
